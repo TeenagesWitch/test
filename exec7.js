29ee0e3c4f5556ef312c1aef9f6bf54e8d6a007d
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-02 13:53:02 +0700

    upd

--- exec7.js
+++ exec7.js
@@ -176,3 +176,40 @@ function romanToInt(s) {
   
 console.log(romanToInt('MCMXCIV'));
 console.log(romanToInt('IV'));
+
+// 06
+// Function to return the longest palindrome in a string
+
+function longestPalindrome(s) {
+    let longest = '';
+
+    for (let i = 0; i < s.length; i++) {
+        let left = i;
+        let right = i;
+
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+
+        if (right - left - 1 > longest.length) {
+            longest = s.substring(left + 1, right);
+        }
+
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+
+        if (right - left - 1 > longest.length) {
+            longest = s.substring(left + 1, right);
+        }
+    }
+
+    return longest;
+}
+
+console.log(longestPalindrome('babad'));
