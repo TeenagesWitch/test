d8404d6ba23cf96517160edea38bbfa9d1cc468e
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-03 11:41:40 +0700

    fix

--- exec8.js
+++ exec8.js
@@ -34,105 +34,109 @@ class ParttimeEmployee extends Employee {
 
 // Example usage:
 const fulltimeEmployee = new FulltimeEmployee();
-fulltimeEmployee.addWorkingHours(7);
-console.log('Fulltime Employee Salary:', fulltimeEmployee.calculateTotalSalary()); // Output: 675000
+fulltimeEmployee.addWorkingHours(3);
+console.log('Fulltime Employee Salary:', fulltimeEmployee.calculateTotalSalary());
+fulltimeEmployee.addWorkingHours(4);
+console.log('Fulltime Employee Salary:', fulltimeEmployee.calculateTotalSalary());
 
 const parttimeEmployee = new ParttimeEmployee();
-parttimeEmployee.addWorkingHours(7);
-console.log('Parttime Employee Salary:', parttimeEmployee.calculateTotalSalary()); // Output: 330000
+parttimeEmployee.addWorkingHours(4);
+console.log('Parttime Employee Salary:', parttimeEmployee.calculateTotalSalary());
+parttimeEmployee.addWorkingHours(3);
+console.log('Parttime Employee Salary:', parttimeEmployee.calculateTotalSalary());
   
-  // 02
-  const readline = require('readline');
+// 02
+const readline = require('readline');
 
-  class Player {
-    constructor(name) {
-      this.name = name;
-      this.health = 100;
-      this.power = 10;
-      this.isComputer = false;
-    }
-  
-    hit(power) {
-      this.health -= power;
-    }
-  
-    useItem(item) {
-      this.health += item.health;
-      this.power += item.power;
-    }
-  
-    showStatus() {
-      return `Player ${this.name} (Health => ${this.health}, Power => ${this.power})`;
-    }
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.power = 10;
+    this.isComputer = false;
   }
-  
-  class ShootingGame {
-    constructor(player1, player2) {
-      this.player1 = player1;
-      this.player2 = player2 || new Player('Computer');
-      this.player2.isComputer = player2 == null;
-    }
-  
-    getRandomItem() {
-      return {
-        health: Math.random() < 0.5 ? 10 : 0,
-        power: Math.random() < 0.5 ? 10 : 0,
-      };
-    }
-  
-    async takeTurn(player, opponent) {
-      console.log(player.showStatus());
-  
-      if (player.isComputer) {
-        let choice = Math.random() < 0.5 ? 'item' : 'attack';
-        if (choice === 'item') {
-          let item = this.getRandomItem();
-          player.useItem(item);
-          console.log(`Player ${player.name} used item: Health +${item.health}, Power +${item.power}`);
-        } else {
-          opponent.hit(player.power);
-          console.log(`Player ${player.name} attacked Player ${opponent.name}`);
-        }
+
+  hit(power) {
+    this.health -= power;
+  }
+
+  useItem(item) {
+    this.health += item.health;
+    this.power += item.power;
+  }
+
+  showStatus() {
+    return `Player ${this.name} (Health => ${this.health}, Power => ${this.power})`;
+  }
+}
+
+class ShootingGame {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2 || new Player('Computer');
+    this.player2.isComputer = player2 == null;
+  }
+
+  getRandomItem() {
+    return {
+      health: Math.random() < 0.5 ? 10 : 0,
+      power: Math.random() < 0.5 ? 10 : 0,
+    };
+  }
+
+  async takeTurn(player, opponent) {
+    console.log(player.showStatus());
+
+    if (player.isComputer) {
+      let choice = Math.random() < 0.5 ? 'item' : 'attack';
+      if (choice === 'item') {
+        let item = this.getRandomItem();
+        player.useItem(item);
+        console.log(`Player ${player.name} used item: Health +${item.health}, Power +${item.power}`);
       } else {
-        const rl = readline.createInterface({
-          input: process.stdin,
-          output: process.stdout,
-        });
-  
-        const question = (prompt) => new Promise((resolve) => rl.question(prompt, resolve));
-  
-        let choice = await question('Do you want to use an item or attack? (item/attack): ');
-  
-        if (choice.toLowerCase() === 'item') {
-          let item = this.getRandomItem();
-          player.useItem(item);
-          console.log(`Player ${player.name} used item: Health +${item.health}, Power +${item.power}`);
-        } else {
-          opponent.hit(player.power);
-          console.log(`Player ${player.name} attacked Player ${opponent.name}`);
-        }
-  
-        rl.close();
+        opponent.hit(player.power);
+        console.log(`Player ${player.name} attacked Player ${opponent.name}`);
       }
-    }
-  
-    async start() {
-      console.log('Game started!');
-  
-      while (this.player1.health > 0 && this.player2.health > 0) {
-        await this.takeTurn(this.player1, this.player2);
-        if (this.player2.health <= 0) break;
-  
-        await this.takeTurn(this.player2, this.player1);
-        console.log('-----------------------------');
+    } else {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      const question = (prompt) => new Promise((resolve) => rl.question(prompt, resolve));
+
+      let choice = await question('Do you want to use an item or attack? (item/attack): ');
+
+      if (choice.toLowerCase() === 'item') {
+        let item = this.getRandomItem();
+        player.useItem(item);
+        console.log(`Player ${player.name} used item: Health +${item.health}, Power +${item.power}`);
+      } else {
+        opponent.hit(player.power);
+        console.log(`Player ${player.name} attacked Player ${opponent.name}`);
       }
-  
-      let winner = this.player1.health > 0 ? this.player1.name : this.player2.name;
-      console.log(`Winner is ${winner}`);
+
+      rl.close();
     }
   }
-  
-  // Example usage:
-  const playerA = new Player('A');
-  const game = new ShootingGame(playerA);
-  game.start();
+
+  async start() {
+    console.log('Game started!');
+
+    while (this.player1.health > 0 && this.player2.health > 0) {
+      await this.takeTurn(this.player1, this.player2);
+      if (this.player2.health <= 0) break;
+
+      await this.takeTurn(this.player2, this.player1);
+      console.log('-----------------------------');
+    }
+
+    let winner = this.player1.health > 0 ? this.player1.name : this.player2.name;
+    console.log(`Winner is ${winner}`);
+  }
+}
+
+// Example usage:
+const playerA = new Player('A');
+const game = new ShootingGame(playerA);
+game.start();
