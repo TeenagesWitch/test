5c3a7f83b2e032999ada81c83e39eae7d4f18dff
TeenagesWitch <hananmuhammadkemal@gmail.com>
2023-08-07 14:12:45 +0700

    upd

--- exec9.js
+++ exec9.js
@@ -11,3 +11,16 @@ function excelColToInt(col) {
 }
     
 console.log(excelColToInt("AZ"));
+
+// 02
+// function to convert integer to roman numeral (fastest time complexity)
+
+function intToRoman(num) {
+    const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const ths = ["", "M", "MM", "MMM"];
+    return ths[Math.floor(num / 1000)] + hrns[Math.floor((num % 1000) / 100)] + tens[Math.floor((num % 100) / 10)] + ones[num % 10];
+}
+
+console.log(intToRoman(3999));
